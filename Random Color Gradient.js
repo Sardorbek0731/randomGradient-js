4fb125f8fa57eaa2407e7c7754036039ffda8ba8
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-08-14 11:51:29 +0500

    randomGradient-js

---
+++ Random Color Gradient.js
@@ -0,0 +1,49 @@
+// Random Color Gradient
+
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const colorText = document.querySelector(".color-text");
+
+let values = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "s",
+  "d",
+  "e",
+  "f",
+];
+
+function getGradient() {
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    let randomNumber = Math.trunc(Math.random() * values.length);
+    color += values[randomNumber];
+  }
+
+  return color;
+}
+
+function setGradient() {
+  const color1 = getGradient();
+  const color2 = getGradient();
+  const randomDeg = Math.trunc(Math.random() * 360);
+  const bgColor = `linear-gradient(
+    ${randomDeg}deg,
+    ${color1},
+    ${color2}
+    )`;
+  body.style.background = bgColor;
+  colorText.textContent = bgColor;
+}
+setGradient();
+
+container.addEventListener('click', setGradient);
